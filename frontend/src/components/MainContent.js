0b3da5fdd6dc8035c925cbafa50bdacee0d24d52
Agustin Boccio <aboccio@hotmail.com>
2022-03-17 18:00:40 +0100

    The array of movements stores the id, operation and amount values

--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -13,45 +13,62 @@ function MainContent() {
         flexDirection: 'column'
     }
 
-    let balance = 1500;
+    let balance = 0;
     let newMovement = useRef();
 
+    const [ radioSelection, setRadioSelection ] = useState([]);
+
+    const [ currentAmount, setAmount ] = useState(0);
+
     const [ movements, setMovement ] = useState([
-        {
-            id:1, 
-            operation: 'income',
-            amount: 1000
-        }
+        // {
+        //     id:1, 
+        //     operation: 'income',
+        //     amount: 1000
+        // }
     ]);
 
-    // Capture de radio selection
-    const radioOperation = (e) => {
-        return e.target.id;
+    // Capture and storage of radio selection changes
+    const handleRadioSelection = (e) => {
+        
+        setRadioSelection( e.target.value );
+
     };
+
+    // Capture and storage amount 
+
 
     // When an amount is entered, first check if the key is 'Enter', then check if the value is not empty, !empty the state is updated
     const handleAmount = (e) => {
         if (e.key !== 'Enter') return
         if ( e.key === 'Enter' ) {
-            if ( newMovement.current.value === '' ) return;
-
-            setMovement( previousMovements => {
-                return [...previousMovements, {id: uuid(), operation: radioOperation, amount: newMovement.current.value}]
-            })
-            console.log(`Apretó ${e.key} con el valor ${newMovement.current.value}`)
-            console.log(movements);
+            if ( newMovement.current.value === '' ) {
+                return
+            } else if ( newMovement.current.value !== '' ) {
+                setAmount (
+                    newMovement.current.value
+                )
+                setMovement( previousMovements => {
+                    return [...previousMovements, {id: uuid(), operation: radioSelection, amount: currentAmount}]
+                })
+                console.log(`Apretó ${e.key} con el valor ${newMovement.current.value}`)
+                console.log(movements);
+                
+            }
+            newMovement.current.value = '';
         }
-        newMovement.current.value = '';
     }
+
+
 
     return (
         <React.Fragment>
             <section>
                 <h2>You balance is: {balance}</h2>
                 <article style={dataEntry}>
-                    <div onChange={radioOperation}>
-                        <input type='radio' name='operation' id='income'></input><label htmlFor='income'>Income</label>
-                        <input type='radio' name='operation' id='outcome'></input><label htmlFor='outcome'>Outcome</label>
+                    <div onChange={handleRadioSelection}>
+                        <input type='radio' name='operation' id='income' value='income'></input><label htmlFor='income'>Income</label>
+                        <input type='radio' name='operation' id='outcome' value='outcome'></input><label htmlFor='outcome'>Outcome</label>
                     </div>
                     <input type='number' ref={newMovement} placeholder='Operation Amount' onKeyDown={handleAmount}></input>
                     <sup>Insert amount and press Return button ⏎</sup>
